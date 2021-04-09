2ed0ea558e524cbe257949171b852f291bd3368f
2ken-eng <734929968@qq.com>
2021-04-09 20:20:43 +0800

    postLocation;重定向请求

---
+++ hystrix/src/main/java/org/ken/hystrix/HystrixApplication.java
@@ -0,0 +1,32 @@
+package org.ken.hystrix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.cloud.client.SpringCloudApplication;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+//@SpringBootApplication
+
+
+@SpringCloudApplication
+public class HystrixApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HystrixApplication.class, args);
+
+
+        }
+
+
+
+
+    @Bean
+    @LoadBalanced
+    RestTemplate restTemplate(){
+
+
+        return new RestTemplate();
+    }
+
+}
